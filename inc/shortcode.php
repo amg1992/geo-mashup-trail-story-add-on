de4b71fabcd48baf55fcbf072bf966469f60a1fa
Andrew Gunn <andrewmgunn26@gmail.com>
2015-10-06 16:10:17 -0500

    Adds frontend trail story map shortcode

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -2,6 +2,8 @@
 //Hey there guy.
 defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 
+add_shortcode( 'trail_story', 'trail_story_shortcode' );
+
 function trail_story_shortcode( $attributes ) {
     $attr = shortcode_atts( array(
         'foo' => 'something',
@@ -11,4 +13,15 @@ function trail_story_shortcode( $attributes ) {
     return "Trail Story shortcode";
 }
 
-add_shortcode( 'trail_story', 'trail_story_shortcode' );
+add_shortcode( 'frontend_trail_story_map', 'frontend_user_map_shortcode' );
+
+function frontend_trail_story_map( $attributes ) {
+    $attr = shortcode_atts( array(
+        'foo' => 'something',
+        'bar' => 'something else',
+    ), $attributes );
+
+    return "Trail Story shortcode";
+}
+
+
