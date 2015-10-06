b6bf5853193695e6aeb38395ba570e65150e9a46
Andrew Gunn <andrewmgunn26@gmail.com>
2015-10-06 11:54:18 -0500

    Adds base for shortcode fucntino

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -1,2 +1,13 @@
 <?php
 //Hey there guy.
+
+function trail_story_shortcode( $attributes ) {
+    $attr = shortcode_atts( array(
+        'foo' => 'something',
+        'bar' => 'something else',
+    ), $attributes );
+
+    return "Trail Story shortcode";
+}
+
+add_shortcode( 'trail_story', 'trail_story_shortcode' );
