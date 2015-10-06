e231f0fe658add7aa39350a2c7ad9696f4471b57
Andrew Gunn <andrewmgunn26@gmail.com>
2015-10-06 11:59:49 -0500

    Adds uninstall.php to delete option data

---
+++ uninstall.php
@@ -0,0 +1,22 @@
+<?php // Get out!
+
+//if uninstall not called from WordPress exit
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
+    exit();
+
+$option_name = 'trail_story_option';
+$setting_name = 'trail_story_settings';
+
+
+delete_option( $option_name );
+delete_option( $setting_name );
+
+// For site options in multisite
+delete_site_option( $option_name );  
+delete_site_option( $setting_name );  
+
+//drop a custom db table
+//global $wpdb;
+//$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}mytable" );
+
+//note in multisite looping through blogs to delete options on each blog does not scale. You'll just have to leave them.
