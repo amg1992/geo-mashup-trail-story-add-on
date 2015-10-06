3e61a04bcd9927f2427926267b7e434319e08755
Ryan Van Ess <fallen.rve@gmail.com>
2015-10-06 15:43:51 -0500

    modifies the post form

--- public/trail-story-post-form.php
+++ public/trail-story-post-form.php
@@ -9,45 +9,42 @@
 defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 
 function trail_story_post_form() {
-    // Create form
-    $post_form = 
-        '<div class="trail-story-form-wrapper">' .
+    // Create form ?>
+        <div class="trail-story-form-wrapper">
 
-            '<form id="new_post" name="new_post" method="post" action="" class="trail-story-form" enctype="multipart/form-data">' .
+            <form id="new_post" name="new_post" method="post" action="" class="trail-story-form" enctype="multipart/form-data">
          
-                '<fieldset name="name">' .
-                    '<label for="title"> ' . _e( 'Trail Story Title', 'geo-mashup-trail-story-add-on' ) . '</label>' .
-                    '<input type="text" id="title" value="" tabindex="5" name="title" />' .
-                '</fieldset>' .
+                <fieldset name="name">
+                    <label for="title"> <?php _e( 'Trail Story Title', 'geo-mashup-trail-story-add-on' ); ?></label>
+                    <input type="text" id="title" value="" tabindex="5" name="title" />
+                </fieldset>
          
-                '<fieldset class="category">' .
-                    '<label for="cat"> ' . _e( 'Trail Story Title', 'geo-mashup-trail-story-add-on' ) . '</label>' .
-                    wp_dropdown_categories( 'tab_index=10&taxonomy=trail-story-category&hide_empty=0' ) .
-                '</fieldset>' .
+                <fieldset class="category">
+                    <label for="cat"> <?php _e( 'Trail Story Chapter', 'geo-mashup-trail-story-add-on' ); ?> </label>
+                    <?php wp_dropdown_categories( 'tab_index=10&taxonomy=trail-story-category&hide_empty=0' ); ?>
+                </fieldset>
              
-                '<fieldset class="content">' .
-                    '<label for="description">Description and Notes:</label>' .
-                    '<textarea id="description" tabindex="15" name="description" cols="80" rows="10"></textarea>' .
-                '</fieldset>' .
+                <fieldset class="content">
+                    <label for="description">Description and Notes:</label>
+                    <textarea id="description" tabindex="15" name="description" cols="80" rows="10"></textarea>
+                </fieldset>
              
-                /*'<fieldset class="tags">' .
-                    '<label for="post_tags">Additional Keywords (comma separated):</label>' .
-                    '<input type="text" value="" tabindex="35" name="post_tags" id="post_tags" />' .
-                '</fieldset>' . */
+                <!--<fieldset class="tags">
+                    <label for="post_tags">Additional Keywords (comma separated):</label>
+                    input type="text" value="" tabindex="35" name="post_tags" id="post_tags" />
+                </fieldset>-->
              
-                '<fieldset class="submit">' .
-                    '<input type="submit" value="Post Review" tabindex="40" id="submit" name="submit" />' .
-                '</fieldset>' .
+                <fieldset class="submit">
+                    <input type="submit" value="Post Review" tabindex="40" id="submit" name="submit" />
+                </fieldset>
              
-                '<input type="hidden" name="action" value="new_post" />' .
-                wp_nonce_field( 'new-post' ) .
+                <input type="hidden" name="action" value="new_post" />
+                wp_nonce_field( 'new-post' );
 
-            '</form>' .
+            </form>
 
-        '</div>';
-    
-    echo $post_form;
-
+        </div>
+<?php
 }
 
 function trail_story_sanitize_post_form_data() {
